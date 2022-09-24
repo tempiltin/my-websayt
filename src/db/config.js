df5f321ed26bc51d4d05debf4bf8c464f9c76c79
tempiltin <temurbekshukurov0707@gmail.com>
2022-09-25 01:43:41 +0500

    asd

--- src/db/config.js
+++ src/db/config.js
@@ -1,23 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSM22LOn16mRjoTkB4eEdELGFswWnnwKg",
-  authDomain: "tempiltin-uz-for-user-auth.firebaseapp.com",
-  projectId: "tempiltin-uz-for-user-auth",
-  storageBucket: "tempiltin-uz-for-user-auth.appspot.com",
-  messagingSenderId: "246017683667",
-  appId: "1:246017683667:web:8519f65ab21fcfef936426"
+  apiKey: process.env.REACT_APP_AUTH_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_AUTH_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_AUTH_MESSANGER_SENDING_ID,
+  appId: process.env.REACT_APP_AUTH_APP_ID
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
- const auth = getAuth(app);
- export default auth;
+const auth = getAuth(app);
+export default auth;
